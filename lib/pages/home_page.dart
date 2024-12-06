25b36e515e113391a873925ce75a624f1e56124d
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2024-12-06 22:58:35 +0530

    feat: setup dependencies

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,10 +5,8 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("data"),
-      ),
+    return const Center(
+      child: Text("data"),
     );
   }
 }
