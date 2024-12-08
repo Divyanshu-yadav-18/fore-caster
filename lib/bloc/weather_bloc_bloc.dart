9c2777d5813b2f67ae4c5e64eccdae939905bcde
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2024-12-08 15:01:16 +0530

    feat: bloc setup

--- lib/bloc/weather_bloc_bloc.dart
+++ lib/bloc/weather_bloc_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
 
 part 'weather_bloc_event.dart';
 part 'weather_bloc_state.dart';
--- lib/bloc/weather_bloc_state.dart
+++ lib/bloc/weather_bloc_state.dart
@@ -1,6 +1,17 @@
 part of 'weather_bloc_bloc.dart';
 
 @immutable
-sealed class WeatherBlocState {}
+sealed class WeatherBlocState {
+  const WeatherBlocState();
+
+  @override
+  List<Object> get props => [];
+}
 
 final class WeatherBlocInitial extends WeatherBlocState {}
+
+final class WeatherBlocLoading extends WeatherBlocState {}
+
+final class WeatherBlocFailure extends WeatherBlocState {}
+
+final class WeatherBlocSuccess extends WeatherBlocState {}
