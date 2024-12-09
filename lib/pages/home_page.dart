9e4e2d8e0054fe9acb911eb57358d419e878b62a
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2024-12-09 21:02:47 +0530

    fix:icons fix

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,8 +7,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:forecaster/bloc/weather_bloc_bloc.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget getWeatherIcon(int code) {
+    switch (code) {
+      case >= 200 && < 300:
+        return Image.asset(
+          'assets/1.png',
+        );
+      case >= 300 && < 400:
+        return Image.asset(
+          'assets/2.png',
+        );
+      case >= 500 && < 600:
+        return Image.asset(
+          'assets/3.png',
+        );
+      case >= 600 && < 700:
+        return Image.asset(
+          'assets/4.png',
+        );
+      case >= 700 && < 800:
+        return Image.asset(
+          'assets/5.png',
+        );
+      case == 800:
+        return Image.asset(
+          'assets/6.png',
+        );
+      case > 800 && <= 804:
+        return Image.asset(
+          'assets/7.png',
+        );
+      default:
+        return Image.asset(
+          'assets/7.png',
+        );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -106,9 +148,7 @@ class MyHomePage extends StatelessWidget {
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                          Image.asset(
-                            'assets/8.png',
-                          ),
+                          getWeatherIcon(state.weather.weatherConditionCode!),
                           Center(
                             child: Text(
                               '${state.weather.temperature!.celsius!.round()}°C',
